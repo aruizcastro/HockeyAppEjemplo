b5229f925cbffe839250aacd824fd41b14a94335
aruiz <aruiz@horbath.com>
2017-02-06 17:52:29 -0500

    primer comit

---
+++ AplicacionEjemplo/AplicacionEjemplo/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AplicacionEjemplo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BotonError.Clicked += (sender, obj) =>
+            {
+                throw new Exception("Aca un error");
+            };
+
+            BotonError.Clicked += (sender, obj) =>
+            {
+                HockeyApp.MetricsManager.TrackEvent("Evento 1");
+                //throw new Exception("Aca un error");
+            };
+
+
+        }
+    }
+}
